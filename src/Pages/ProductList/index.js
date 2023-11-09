a1ee5821cdfe8b21889389a9930291a4c91da959
photonrays <lthang052@gmail.com>
2023-11-09 23:20:57 +0700

    add template change button

--- src/Pages/ProductList/index.js
+++ src/Pages/ProductList/index.js
@@ -4,19 +4,12 @@ import DetailSection from './Components/DetailSection';
 import ProductSection from './Components/ProductSection';
 import TopSection from './Components/TopSection';
 import { useEffect } from 'react';
-import { useMultiContext } from '../../contexts/multiContext';
 
 export default function ProductList({ type }) {
-    const {theme, setTheme} = useMultiContext()
-
-    console.log(theme)
-
     return (
         <div className='w-full bg-[#f5f5f7]'>
             <TopSection type={type} />
             <Banner type={type} />
-            {/* <button className='w-52 h-10 bg-slate-300 mx-auto' onClick={() => setTheme("dark")}>Dark theme</button>
-            <button className='w-52 h-10 bg-slate-300 mx-auto' onClick={() => setTheme("light")}>Light theme</button> */}
             <ProductSection type={type} />
             <DetailSection type={type} />
             {/* <SubcribeSection /> */}
