279f1cd8bd792e6347f74299f28625728fab39cc
photonrays <lthang052@gmail.com>
2023-12-06 20:52:50 +0700

    add template context

--- src/Pages/ProductList/index.js
+++ src/Pages/ProductList/index.js
@@ -4,8 +4,11 @@ import DetailSection from './Components/DetailSection';
 import ProductSection from './Components/ProductSection';
 import TopSection from './Components/TopSection';
 import { useEffect } from 'react';
+import { useTemplateContext } from '../../contexts/templateContext';
 
 export default function ProductList({ type }) {
+    const {template} = useTemplateContext();
+    console.log("(Product) template: ", template)
     return (
         <div className='w-full bg-[#f5f5f7]'>
             <TopSection type={type} />
--- src/contexts/templateContext.js
+++ src/contexts/templateContext.js
@@ -1,11 +1,18 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useState } from "react";
 import { createContext } from "react";
 
 export const TemplateContext = createContext(null);
 
 export function TemplateContextProvider({ children }) {
-  const [template, setTemplate] = useState(1);
+  const [template, setTemplate] = useState(() => {
+    const template = sessionStorage.getItem("template");
+    return JSON.parse(template) || "1"
+  });
+
+  useEffect(() => {
+    sessionStorage.setItem("template", JSON.stringify(template))
+  }, [template])
 
   return (
     <TemplateContext.Provider value={{ template, setTemplate }}>
