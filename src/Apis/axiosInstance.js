b44537f0f3c3f1765d78bfe3c3783b68651cf31f
photonrays <lthang052@gmail.com>
2024-05-03 22:12:10 +0700

    remove subiz

--- src/Apis/axiosInstance.js
+++ src/Apis/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001/",
+    baseURL: "https://relieved-cummerbund-eel.cyclic.app/",
     // baseURL: "/",
     headers: {
         "Content-Type": "application/json",
