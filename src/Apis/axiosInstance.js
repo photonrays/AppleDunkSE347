09aa2f4de65e41904c7a30a318a7d863da1f613f
photonrays <lthang052@gmail.com>
2024-05-31 21:24:23 +0700

    update api

--- src/Apis/axiosInstance.js
+++ src/Apis/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-    baseURL: "https://relieved-cummerbund-eel.cyclic.app/",
+    baseURL: "https://appledunkse347be.onrender.com/",
     // baseURL: "/",
     headers: {
         "Content-Type": "application/json",
